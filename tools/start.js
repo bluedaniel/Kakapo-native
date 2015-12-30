86763b7843929b304697ff10cb96e783638ac025
Daniel Levitt <daniel.levitt@guardian.co.uk>
2015-12-30 13:09:55 +0000

    Update assets

--- tools/start.js
+++ tools/start.js
@@ -7,7 +7,7 @@ export default async function start() {
   await run(require('./copy'));
 
   if (argv.platform === 'android') {
-    proc.spawn('react-native' , [ 'run-android' ])
+    proc.spawn('react-native', [ 'run-android' ])
       .stdout.on('data', data => console.log(data.toString()));
   } else {
     proc.spawn('node_modules/react-native/packager/packager.sh')
