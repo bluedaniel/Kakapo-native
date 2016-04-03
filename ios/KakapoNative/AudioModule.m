2d9344fe20e73c55a437e02fc5628576850128f8
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-04-03 20:16:22 +0100

    More iOS updating
    Removing tools/ as they are no longer really needed

--- ios/KakapoNative/AudioModule.m
+++ ios/KakapoNative/AudioModule.m
@@ -5,8 +5,7 @@
 //  Created by Daniel Levitt on 03/10/2015.
 //  Copyright © 2015 SuperCerebral. All rights reserved.
 //
-#import "kakapo-Bridging-Header.h"
-
+#import "KakapoNative-Bridging-Header.h"
 @interface RCT_EXTERN_MODULE(AudioModule, NSObject)
 
 RCT_EXTERN_METHOD(setSound:(NSString *)sound vol:(nonnull NSNumber *)vol)
--- tools/copy.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs-extra';
-
-export default async function copy() {
-  await fs.copy('node_modules/kakapo-assets/images', 'android/app/src/main/res/drawable-mdpi', {});
-  await fs.copy('node_modules/kakapo-assets/sounds', 'android/app/src/main/res/raw', {});
-}
