e37fe5c58c5f97d07d2bb21abb5cd156965c941c
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-04-05 23:18:16 +0100

    KDSocialShare gone

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,79 +1,26 @@
-import React, {
-  NativeModules, Linking, ScrollView, ToastAndroid, AlertIOS,
-  TouchableOpacity, Text, View, Platform
-} from 'react-native';
+import React, { Linking, ScrollView, TouchableOpacity, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Color from 'color';
 import ColorPicker from './colorPicker';
 import styles from '../styles/settings';
 
-const { KDSocialShare } = NativeModules;
-
-const shareiOS = {
-  text: 'Kakapo on the App Store',
-  link: 'https://itunes.apple.com/us/app/kakapo/id1046673139',
-  imagelink: 'http://a2.mzstatic.com/eu/r30/Purple3/v4/12/ab/2a/12ab2a01-3a3c-9482-b8df-ab38ad281165/icon175x175.png'
-};
-const shareAndroid = Object.assign({}, shareiOS, {
-  text: 'Kakapo on the Play Store',
-  link: 'https://play.google.com/store/apps/details?id=com.kakaponative'
-});
-
-const githubRepo = 'https://github.com/bluedaniel/Kakapo-native';
+const shareText = encodeURI('Kakapo Apps - Ambient sound mixer for relaxation or productivity');
+const shareLink = 'http://kakapo.co/app.html';
+const githubLink = 'https://github.com/bluedaniel/Kakapo-native';
+const facebookLink = `http://www.facebook.com/sharer/sharer.php?u=${shareLink}&t=${shareText}`;
+const twitterLink = `https://twitter.com/intent/tweet?text=${shareText}&url=${shareLink}`;
 
 export default ({ themes, dispatch }) => {
-  const tweet = () => KDSocialShare.tweet(shareiOS, res => res === 'not_available' ?
-      AlertIOS.alert('Twitter not available', 'Setup Twitter in Settings > Twitter') : null);
-
-  const facebook = () => KDSocialShare.shareOnFacebook(shareiOS, res => res === 'not_available' ?
-    AlertIOS.alert('Facebook not available', 'Setup Facebook in Settings > Facebook') : null);
-
-  const shareIntent = () => KDSocialShare.shareIntent(shareAndroid, res => res === 'not_available' ?
-    ToastAndroid.show('Sharing not available :(', ToastAndroid.SHORT) : null);
-
-  const openURL = () => {
-    if (Platform.OS === 'ios') {
-      Linking.openURL(githubRepo);
-    } else {
-      KDSocialShare.openURL(githubRepo);
-    }
-  };
-
-  const renderShareios = () => (
-    <View>
-      <TouchableOpacity style={styles.optWrap} onPress={facebook}>
-        <Icon
-          name="share"
-          size={30}
-          color="#fff"
-          style={styles.optWrapIcon}
-        />
-        <Text style={styles.opt}>Share on Facebook</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.optWrap} onPress={tweet}>
-        <Icon
-          name="share"
-          size={30}
-          color="#fff"
-          style={styles.optWrapIcon}
-        />
-        <Text style={styles.opt}>Share on Twitter</Text>
-      </TouchableOpacity>
-    </View>
-  );
-
-  const renderShareAndroid = () => (
-    <View>
-      <TouchableOpacity style={styles.optWrap} onPress={shareIntent}>
-        <Icon
-          name="share"
-          size={30}
-          color="#fff"
-          style={styles.optWrapIcon}
-        />
-      <Text style={styles.opt}>Share Kakapo</Text>
-      </TouchableOpacity>
-    </View>
+  const share = (link, icon, text) => (
+    <TouchableOpacity style={styles.optWrap} onPress={() => Linking.openURL(link)}>
+      <Icon
+        name={icon}
+        size={30}
+        color="#fff"
+        style={styles.optWrapIcon}
+      />
+      <Text style={styles.opt}>{text}</Text>
+    </TouchableOpacity>
   );
 
   return (
@@ -88,19 +35,9 @@ export default ({ themes, dispatch }) => {
       <Text style={styles.opt}>Color</Text>
       <ColorPicker { ...{ dispatch, color: themes.get('palette').first() }} />
       <Text style={styles.header}>Extra</Text>
-      {Platform.OS === 'ios' ? renderShareios() : renderShareAndroid()}
-      <TouchableOpacity
-        style={styles.optWrap}
-        onPress={openURL}
-      >
-        <Icon
-          name="github"
-          size={30}
-          color="#fff"
-          style={styles.optWrapIcon}
-        />
-        <Text style={styles.opt}>Fork on GitHub!</Text>
-      </TouchableOpacity>
+      {share(facebookLink, 'share', 'Share on Facebook')}
+      {share(twitterLink, 'share', 'Share on Twitter')}
+      {share(githubLink, 'github', 'Fork on GitHub!')}
     </ScrollView>
   );
 };
